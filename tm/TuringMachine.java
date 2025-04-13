898020a361b309f475cc1e6d17bb862fd91c17a5
Tony Hernandez <tonyhernandez24@u.boisestate.edu>
2025-04-13 13:06:50 -0600

    Began working on TuringMachine

--- tm/TuringMachine.java
+++ tm/TuringMachine.java
@@ -1,10 +1,20 @@
 package tm;
 
+import java.util.Map;
+
 /**
  * TuringMachine a representation of a turing machine
  * 
  * @author Zach johnson, Antonio hernandez
  */
 public class TuringMachine {
+    // Instance Variables
+    Map<Integer, TMState> machineStates;
+
+    // Constructor
+    public TuringMachine() {
+
+    }
+    // Methods
 
 }
