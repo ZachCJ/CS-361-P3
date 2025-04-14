53c6659f889ef43f879e867e566950f3ff3777f5
Tony Hernandez <tonyhernandez24@u.boisestate.edu>
2025-04-13 18:29:41 -0600

    Began working on TuringMachine.java

--- tm/TuringMachine.java
+++ tm/TuringMachine.java
@@ -1,6 +1,11 @@
 package tm;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+
+import tm.TMState;
+import tm.TMTape;
 
 /**
  * TuringMachine a representation of a turing machine
@@ -9,12 +14,50 @@ import java.util.Map;
  */
 public class TuringMachine {
     // Instance Variables
-    Map<Integer, TMState> machineStates;
+    private Map<Integer, TMState> machineStates; // All states in machine
+    private ArrayList<String> allMachineTransitions;
+    private TMTape machineTape; // Tape associated with the machine
+    private TMState currentState;
+    private TMState haltingState;
 
     // Constructor
-    public TuringMachine() {
+    public TuringMachine(int numStates, int numSymbols, ArrayList<String> allMachineTransitions) {
+        this.machineStates = new HashMap<>();
+        this.allMachineTransitions = allMachineTransitions;
 
+        // Initialize states
+        for (int i = 0; i < numStates; i++) {
+            TMState newState = new TMState(i);
+            machineStates.put(i, newState);
+        }
+        // add each states transitions
     }
-    // Methods
+
+    // Class Methods
+
+    /**
+     * Returns a string representation of the Turing Machine,
+     * including the current state, halting state, tape contents,
+     * and all state transitions.
+     *
+     * @return a string describing the machine's configuration and transitions
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("=== Turing Machine ===\n");
+        sb.append("Current State: ").append(currentState != null ? currentState.getStateName() : "null").append("\n");
+        sb.append("Halting State: ").append(haltingState != null ? haltingState.getStateName() : "null").append("\n");
+        sb.append("Tape: ").append(machineTape != null ? machineTape.toString() : "null").append("\n\n");
+
+        sb.append("States and Transitions:\n");
+        for (Map.Entry<Integer, TMState> entry : machineStates.entrySet()) {
+            TMState value = entry.getValue();
+            sb.append(value.toString());
+        }
+
+        return sb.toString();
+    }
 
 }
