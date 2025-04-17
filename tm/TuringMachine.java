72a9ddd1236eee0f8b661c0ea34903db3d6f5954
Tony Hernandez <tonyhernandez24@u.boisestate.edu>
2025-04-17 12:02:20 -0600

    Added a step() for running simulation

--- tm/TuringMachine.java
+++ tm/TuringMachine.java
@@ -16,23 +16,68 @@ public class TuringMachine {
     // Instance Variables
     private ArrayList<TMState> machineStates;
     private Map<String, Transition[]> allMachineTransitions; // All the transitions in the machine
-    private TMTape machineTape; // Tape associated with the machine
+    public TMTape machineTape; // Tape associated with the machine
     private TMState currentState; // Where the head is pointing at
     private TMState haltingState; // Halting state should be the n-1 state (Last state in machine)
 
     // Constructor
     public TuringMachine(ArrayList<TMState> machineStates, int numSymbols,
-            Map<String, Transition[]> allMachineTransitions) {
+            Map<String, Transition[]> allMachineTransitions, String intputString) {
 
         this.machineStates = machineStates;
         this.allMachineTransitions = allMachineTransitions;
-        this.machineTape = new TMTape();
+        this.machineTape = new TMTape(intputString);
         this.currentState = machineStates.get(0);
         this.haltingState = machineStates.get(machineStates.size() - 1);
 
     }
 
     // Class Methods
+    public boolean step() {
+        int currentSymbol = machineTape.read();
+        // DEBUG:
+        System.out.println("Current State: " + currentState.getStateName());
+
+        Transition[] transitions = allMachineTransitions.get(Integer.toString(currentState.getStateName()));
+        // DEBUG:
+        System.out.println("Current Symbol Transitions: ");
+        for (Transition transition : transitions) {
+            System.out.println(transition);
+        }
+
+        // If there are not transitions should be in halting state
+        if (transitions == null) {
+            return true;
+
+        }
+        // Get next transition based on current symbol
+        Transition nextTransition = transitions[currentSymbol];
+        // DEBUG:
+        System.out.println("Transition: " + nextTransition.toString());
+
+        // writing to tape:
+        machineTape.write(Integer.parseInt(nextTransition.getWriteSymbol()));
+        // DEBUG
+        System.out.println("Writing: " + nextTransition.getWriteSymbol());
+
+        // move head
+        if (nextTransition.getMove() == 'L') {
+            machineTape.move('L');
+        } else {
+            machineTape.move('R');
+        }
+        // DEBUG
+        System.out.println("Moving: " + nextTransition.getMove());
+
+        // Change state
+        currentState = machineStates.get(Integer.parseInt(nextTransition.getNextState()));
+        // DEBUG
+        System.out.println("Updated State: " + currentState.getStateName());
+
+        // Halt if in halting state
+        return currentState.getStateName() == haltingState.getStateName();
+
+    }
 
     /**
      * Returns a string representation of the Turing Machine,
